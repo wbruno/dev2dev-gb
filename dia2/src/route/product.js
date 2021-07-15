107d91f2be6dbe88beae7b8e8713663a388a655a
William Bruno <wbrunom@gmail.com>
2021-07-15 16:39:34 -0300

    feat: add dia3 serverless

--- dia2/src/route/product.js
+++ dia2/src/route/product.js
@@ -1,5 +1,8 @@
+// src/route/Product.js
+
 const schema = require('../schema/Product');
 const controller = require('../controller/ProductController');
+const Product = require('../schema/Product');
 
 const route = {
     create: {
